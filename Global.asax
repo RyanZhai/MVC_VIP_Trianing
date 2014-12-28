59e5b141a2d9d60fa968c6ae27865187bdf76611
RyanZhai <101606123@qq.com>
2014-12-28 23:33:39 +0800

    Revert "Revert "创建项目""
    This reverts commit 319db8ba394bc0bb37ff371ae01cf0ab892bd087.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_VIP_Trianing.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_VIP_Trianing.Startup))]
+namespace MVC_VIP_Trianing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
